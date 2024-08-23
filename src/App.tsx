b34cbabcf32e2185d00610888b2cc1203b201af7
Pedro Clerici <pedro.clerici@protonmail.com>
2024-08-23 17:49:51 -0300

    feat: added header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
+import { Header } from "./components/Header";
 
 export function App() {
 	return (
 		<ThemeProvider theme={defaultTheme}>
 			<GlobalStyle />
 
-			<h1>Hello, world!</h1>
+			<Header />
 		</ThemeProvider>
 	);
 }
